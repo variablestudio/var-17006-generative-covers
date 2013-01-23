d13b55be28958444be42637abbc133957591f8bd
Vorg <marcin.ignac@gmail.com>
2013-01-23 20:30:14 +0000

    Crayons font measurements and drawing functions

--- js/crayons.js
+++ js/crayons.js
@@ -93,7 +93,7 @@ HTMLCanvasCrayon.prototype.createStyle = function() {
     color: [0, 0, 0, 255],
     stroke: true,
     fill: false,
-    fontName: "Arial",
+    fontFamily: "Arial",
     fontSize: 12,
     textLeading: 0 //the same as fontSize
   };
@@ -150,11 +150,12 @@ HTMLCanvasCrayon.prototype.stroke = function(enabled) {
 };
 
 //fontSize in px
-HTMLCanvasCrayon.prototype.font = function(fontName, fontSize, textLeading) {
+HTMLCanvasCrayon.prototype.font = function(fontFamily, fontSize, textLeading) {
   if (typeof textLeading === "undefined") textLeading = 0;
-  this.currentStyle.fontName = fontName;
+  this.currentStyle.fontFamily = fontFamily;
   this.currentStyle.fontSize = Math.floor(fontSize);
   this.currentStyle.textLeading = textLeading;
+  return this;
 };
 
 HTMLCanvasCrayon.prototype.beforeDraw = function() {
@@ -168,8 +169,8 @@ HTMLCanvasCrayon.prototype.beforeDraw = function() {
     this.context.strokeStyle = this.currentStyle.color;
   }
 
-  if (this.currentStyle.fontName && this.currentStyle.fontSize) {
-    this.context.font = this.currentStyle.fontSize + "px" + " " + this.currentStyle.fontName;
+  if (this.currentStyle.fontFamily && this.currentStyle.fontSize) {
+    this.context.font = this.currentStyle.fontSize + "px" + " " + this.currentStyle.fontFamily;
   }
 
   this.transformStack.forEach(function(transform) {
@@ -225,39 +226,31 @@ HTMLCanvasCrayon.prototype.line = function(x1, y1, x2, y2) {
 HTMLCanvasCrayon.prototype.text = function(str, x, y) {
   this.beforeDraw();
 
-  if (this.currentStyle.fill) {
-    this.context.fillText(str, x, y);
+  if (Object.prototype.toString.call(str) === '[object Array]') {
+    var dy = y;
+    if (this.currentStyle.fill) {
+      str.forEach(function(line) {
+        this.context.fillText(line, x, dy);
+        dy += this.currentStyle.fontSize * (1.0 + this.currentStyle.textLeading);
+      }.bind(this));
+    }
+    if (this.currentStyle.stroke) {
+      this.context.strokeText(str, x, y);
+    }
   }
-  if (this.currentStyle.stroke) {
-    this.context.strokeText(str, x, y);
+  else {
+    if (this.currentStyle.fill) {
+      this.context.fillText(str, x, y);
+    }
+    if (this.currentStyle.stroke) {
+      this.context.strokeText(str, x, y);
+    }
   }
 
   this.afterDraw();
+  return this;
 };
 
-//assumes that beforeDraw() was called and context has current font style set up
-HTMLCanvasCrayon.prototype.textBlockSplitLines = function(str, maxWidth) {
-  //var width = measureTextContext.measureText(text).width;
-  var words = str.split(" ");
-  var lines = [];
-  var currentLine = "";
-  while(words.length > 0) {
-    var word = words.shift();
-    var newLine = currentLine;
-    if (newLine.length > 0) newLine += " ";
-    newLine += word;
-    var measurements = this.context.measureText(newLine);
-    if (measurements.width > maxWidth) {
-      lines.push(currentLine);
-      currentLine = word;
-    }
-    else {
-      currentLine = newLine;
-    }
-  }
-  lines.push(currentLine);
-  return lines;
-};
 
 HTMLCanvasCrayon.prototype.textBlock = function(str, x, y, width) {
   this.beforeDraw();
@@ -268,7 +261,7 @@ HTMLCanvasCrayon.prototype.textBlock = function(str, x, y, width) {
     lines = str;
   }
   else {
-    lines = this.textBlockSplitLines(str, width);
+    lines = this.textBlockSplitLines(str.trim(), width);
   }
 
   var dy = y;// + this.currentStyle.fontSize;
@@ -282,6 +275,10 @@ HTMLCanvasCrayon.prototype.textBlock = function(str, x, y, width) {
   if (this.currentStyle.stroke) {
     this.context.strokeText(str, x, y);
   }
+
+  this.context.strokeStyle = "#FF0000";
+  //fontSize:44 bboxHeight:49 bboxWidth:130 ascent:40 descent:10 emHeight:47 enHeight:23.5
+  this.context.strokeRect(x, y - 40, x + width, lines.length * 49 + 10);
 
   this.afterDraw();
 
@@ -322,16 +319,116 @@ HTMLCanvasCrayon.prototype.scale = function(x, y) {
 HTMLCanvasCrayon.prototype.save = function() {
   var savedStack = this.transformStack.map(function(t) { return t; });
   this.savedTransformStacks.push(savedStack);
+  return this;
 };
 
 HTMLCanvasCrayon.prototype.restore = function() {
   if (this.savedTransformStacks.length > 0) {
     this.transformStack = this.savedTransformStacks.pop();
   }
+  return this;
 };
 
 HTMLCanvasCrayon.prototype.reset = function() {
   this.transformStack = [];
+  return this;
+};
+
+HTMLCanvasCrayon.prototype.measureText = function(str) {
+  var metrics = this.getFontMetrics(this.currentStyle.fontFamily, this.currentStyle.fontSize);
+  var oldFont = this.context.font;
+  this.context.font = this.currentStyle.fontSize + "px " + this.currentStyle.fontFamily;
+  var width = this.context.measureText(str).width;
+  this.context.font = oldFont;
+  return {
+    x : 0,
+    y : -metrics.ascent,
+    width : width,
+    height : metrics.bboxHeight
+  };
+};
+
+//Based on code from http://mudcu.be/journal/2011/01/html5-typographic-metrics/ by Michael Deal
+HTMLCanvasCrayon.prototype.getFontMetrics = function(fontFamily, fontSize) {
+  var container;
+  var control;
+  var image;
+
+  var direction = ""; // ltr, or rtl
+  var whois = "jiraffe!|";
+  var bboxHeight = 0; // size of text bounding-height
+  var bboxWidth = 0; // size of text bounding-width
+  var ascent = 0;
+  var descent = 0;
+  var emHeight = 0; // size of em-height (via measuring offsetTop of element below line-height=0)
+  var enHeight = 0; // size of en-height
+
+  direction = window.getComputedStyle(document.body, "")["direction"];
+
+  // setting up html used for measuring text-metrics
+  container = document.createElement("div");
+  container.style.fontFamily = fontFamily;
+  container.style.fontSize = fontSize + "px";
+  container.style.position = "absolute";
+  container.style.left = 0;
+  container.style.top = 0;
+  //document.body.insertBefore(container, document.body.childNodes[0]);
+  document.body.appendChild(container);
+
+  control = document.createElement("span");
+  image = document.createElement("img");
+  image.width = fontSize;
+  image.height = 1;
+
+  var canvas = document.createElement("canvas");
+  canvas.width = 1;
+  canvas.height = 1;
+  var c = canvas.getContext("2d");
+  c.fillStyle = "#FF0000";
+  c.fillRect(0, 0, 100, 100);
+  image.src = canvas.toDataURL();
+
+  control.appendChild(document.createTextNode(whois));
+  control.appendChild(image);
+  container.appendChild(control);
+
+  // getting css equivalent of ctx.measureText()
+  image.style.display = "none";
+  control.style.display = "inline";
+  bboxHeight = control.offsetHeight;
+  bboxWidth = control.offsetWidth;
+
+  // making sure super-wide text doesn't wrap
+  image.style.display = "inline";
+  var forceWidth = bboxWidth + image.offsetWidth;
+
+  // capturing the "top" and "bottom" baseline
+  control.style.cssText = "margin: " + fontSize + "px 0; display: block; width: " + forceWidth + "px";
+  var TopBaseline = image.offsetTop - fontSize + 1;
+  var HeightCSS = control.offsetHeight;
+  var BottomBaseline = TopBaseline - HeightCSS;
+
+  // capturing the "middle" baseline
+  control.style.cssText = "line-height: 0; display: block; width: " + forceWidth + "px";
+  var MiddleBaseline = image.offsetTop + 1;
+
+  // calculate "em" and "en" height
+  emHeight = (MiddleBaseline - 0.5) * 2;
+  enHeight = emHeight / 2;
+
+  // calculating the ascent and descent
+  descent = -BottomBaseline;
+  ascent = TopBaseline;
+
+  document.body.removeChild(container);
+
+  return {
+    bboxHeight : bboxHeight,
+    descent : descent,
+    ascent : ascent,
+    emHeight : emHeight,
+    enHeight : enHeight
+  };
 };
 
 (function() {
@@ -343,5 +440,4 @@ HTMLCanvasCrayon.prototype.reset = function() {
     console.log("HTMLCanvasCrayon disabled");
   }
 })();
-
 
