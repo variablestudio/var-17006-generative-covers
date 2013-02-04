b783695035c584e536a556ed7053052a5c419c85
Vorg <marcin.ignac@gmail.com>
2013-02-04 16:14:09 +0000

    Added ellipse drawing to crayons

--- js/crayons.js
+++ js/crayons.js
@@ -186,6 +186,34 @@ HTMLCanvasCrayon.prototype.circle = function(x, y, r) {
   return this;
 };
 
+HTMLCanvasCrayon.prototype.ellipse = function(x, y, w, h) {
+  this.beforeDraw();
+
+  //Based on http://stackoverflow.com/a/2173084
+  var kappa = .5522848;
+      ox = (w / 2) * kappa, // control point offset horizontal
+      oy = (h / 2) * kappa, // control point offset vertical
+      xe = x + w,           // x-end
+      ye = y + h,           // y-end
+      xm = x + w / 2,       // x-middle
+      ym = y + h / 2;       // y-middle
+
+  this.context.beginPath();
+  this.context.moveTo(x, ym);
+  this.context.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+  this.context.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+  this.context.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+  this.context.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+  this.context.closePath();
+  if (this.currentStyle.fill) {
+    this.context.fill();
+  }
+  if (this.currentStyle.stroke) {
+    this.context.stroke();
+  }
+  this.afterDraw();
+  return this;
+};
 
 HTMLCanvasCrayon.prototype.line = function(x1, y1, x2, y2) {
   this.beforeDraw();
