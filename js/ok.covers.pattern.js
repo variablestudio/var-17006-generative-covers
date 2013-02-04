183c3d10985fb8a4316aa77667ca059da559b87b
Vorg <marcin.ignac@gmail.com>
2013-02-04 16:14:53 +0000

    Pattern final

--- js/ok.covers.pattern.js
+++ js/ok.covers.pattern.js
@@ -11,15 +11,20 @@ OK.Covers.push((function() {
 
     crayon.style("default");
 
+    var borderScale = 1 + Math.random();
+    var hue = Math.random() * 360;
+    var numX = 2 + Math.floor(Math.random() * 10);
+    var radius = (0.2 + 0.5 * Math.random());
+    var radius2 = (0.1 + 0.4 * Math.random());
+    var shape = Math.floor(Math.random() * 2);
+
     var margins = 0;
 
     crayon.clear();
 
     var niceBlue = "#27D1E7";
     var paleYellow = "rgb(255, 255, 255)";
-
     var colorHSL = chroma.hex(niceBlue).hsl();
-    var hue = Math.random() * 255;
     var color = chroma.hsl(hue, 0.8, colorHSL[2]).hex();
     var darkColor = chroma.hsl(hue, 0.8, colorHSL[2] * 0.8).hex();
     var lightColor = chroma.hsl(hue, 0.8, colorHSL[2] * 1.5).hex();
@@ -29,41 +34,44 @@ OK.Covers.push((function() {
 
     crayon.fill(color).rect(margins, margins, crayon.canvas.width - 2 * margins, crayon.canvas.height - 2 * margins);
 
-    var numX = 5 + Math.floor(Math.random() * 10);
     var stepX = crayon.canvas.width / (numX + 1);
     var stepY = stepX;
-    //var step = 10 + Math.random() * 20;
-    var r = stepX * (0.2 + 0.4 * Math.random());
-    var r2 = 0.25 * stepX * (0.2 + 0.4 * Math.random());
+    var r = stepX * radius;
+    var r2 = stepX * radius2;
 
-    crayon.fill(lightColor);
+    if (shape == 0) r2 *= 0.25; //square
+    if (shape == 1) r2 *= 0.25; //ellipse
+
+    crayon.fill(lightColor).stroke(false);
 
     for(var x=0; x<=crayon.canvas.width + stepX/2; x+=stepX) {
       for(var y=0; y<=crayon.canvas.height + stepY/2; y+=stepY) {
-        crayon
-          .save()
-          .translate(x, y)
-          .rotate(-45)
-          .scale(2, 2)
-          .rect(-r, -r2, r*2, r2*2)
-          .rotate(90)
-          .rect(-r, -r2, r*2, r2*2)
-          .restore();
+        crayon.save();
+        crayon.translate(x, y);
+        crayon.rotate(-45);
+        crayon.scale(borderScale, borderScale);
+        if (shape == 0) crayon.rect(-r, -r2, r*2, r2*2);
+        if (shape == 1) crayon.ellipse(-r, -r2, r*2, r2*2);
+        crayon.rotate(90);
+        if (shape == 0) crayon.rect(-r, -r2, r*2, r2*2);
+        if (shape == 1) crayon.ellipse(-r, -r2, r*2, r2*2);
+        crayon.restore();
       }
     }
 
-    crayon.fill(darkColor);
+    crayon.fill(color).stroke(false);
 
     for(var x=0; x<=crayon.canvas.width + stepX/2; x+=stepX) {
       for(var y=0; y<=crayon.canvas.height + stepY/2; y+=stepY) {
-        crayon
-          .save()
-          .translate(x, y)
-          .rotate(-45)
-          .rect(-r, -r2, r*2, r2*2)
-          .rotate(90)
-          .rect(-r, -r2, r*2, r2*2)
-          .restore();
+        crayon.save();
+        crayon.translate(x, y);
+        crayon.rotate(-45);
+        if (shape == 0) crayon.rect(-r, -r2, r*2, r2*2);
+        if (shape == 1) crayon.ellipse(-r, -r2, r*2, r2*2);
+        crayon.rotate(90);
+        if (shape == 0) crayon.rect(-r, -r2, r*2, r2*2);
+        if (shape == 1) crayon.ellipse(-r, -r2, r*2, r2*2);
+        crayon.restore();
       }
     }
 
