fa441b37d90c347d42e4efcb7752d8ecff34ea8d
Vorg <marcin.ignac@gmail.com>
2013-05-16 17:38:06 +0100

    Working kaleidoscope cover

--- js/ok.covers.kaleidoscope.js
+++ js/ok.covers.kaleidoscope.js
@@ -28,20 +28,45 @@ OK.Covers.push((function() {
     var subTitle = titleSections[1];
 
     crayon.style("drawing");
-    crayon.clip(function(context) {
-      context.beginPath();
-      context.rect(0, 0, 100, crayon.canvas.height);
-      context.clip();
-    })
-    crayon.stroke("#FF0000", 50).fill(false).circle(crayon.canvas.width/2, crayon.canvas.height/2, crayon.canvas.width/2 - 25);
+    //crayon.save();
+    var nx = 2 + Math.floor(Math.random() * 5);
+    var ny = 1 + Math.floor(Math.random() * 5);
+    var thickness = 25 + Math.random() * 25;
+    for(var i=0; i<nx; i++) {
+      for(var j=0; j<ny; j++) {
+        var dx = crayon.canvas.width / nx;
+        var dy = crayon.canvas.height / ny;
+        crayon.clipRect(dx * i, dy * j, dx, crayon.canvas.height / ny);
+        dx -= 20;
+        dy -= 20;
+        //dx += (Math.random() - 0.5) * crayon.canvas.width * 0.3;;
+        var ddy = 0;//(Math.random() - 0.5) * crayon.canvas.height * 0.3;
+        crayon.stroke("#FF0000", thickness).fill(false).circle(crayon.canvas.width/2 + dx * i, crayon.canvas.height/2 + dy * j, crayon.canvas.width/2 - 25);
+        crayon.stroke("#FFFFFF", thickness).fill(false).circle(crayon.canvas.width/2 + dx * i + 50, crayon.canvas.height/2 + dy * j + ddy, crayon.canvas.width/2 - 25);
+      }
+    }
+    //crayon.restore();
 
     crayon.save();
     crayon.translate(titleX, titleY);
     crayon.style("text");
-    crayon.font("Arial", titleFontSize, "bold").fill("#FFFFFF").paragraph("left", 0.25, titleWidth, true).text(title.toUpperCase());
-    crayon.font("Arial", titleFontSize*0.85, "normal").fill("#FFFFFF").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
+    crayon.font("Arial", titleFontSize, "bold").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(title.toUpperCase());
+    crayon.font("Arial", titleFontSize*0.85, "normal").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
+    crayon.font("Arial", authorFontSize).fill("#222222").paragraph("left", 0.25, titleWidth, true).text(author, 0, authorFontSize/2);
+    var offsetY = crayon.translateY;
+    crayon.restore();
+
+    crayon.style("debug").fill("#FFFFFF").rect(0, 0, crayon.canvas.width, offsetY - titleY + titleFontSize*1.5);
+
+    crayon.save();
+    crayon.translate(titleX, titleY);
+    crayon.style("text");
+    crayon.font("Arial", titleFontSize, "bold").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(title.toUpperCase());
+    crayon.font("Arial", titleFontSize*0.85, "normal").fill("#999").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
     crayon.font("Arial", authorFontSize).fill("#222222").paragraph("left", 0.25, titleWidth, true).text(author, 0, authorFontSize/2);
     crayon.restore();
+
+
 
     OK.Covers.Utils.addCover();
   }
