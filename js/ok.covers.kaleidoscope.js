9b0d9b752e391515481404cc5a1fbcf84cd441c5
Vorg <marcin.ignac@gmail.com>
2013-05-16 20:48:48 +0100

    Changed kaleidoscope to lines

--- js/ok.covers.kaleidoscope.js
+++ js/ok.covers.kaleidoscope.js
@@ -30,17 +30,26 @@ OK.Covers.push((function() {
     crayon.style("drawing");
     //crayon.save();
     var nx = 2 + Math.floor(Math.random() * 10);
-    var ny = 1 + Math.floor(Math.random() * 3);
+    var ny = 1;
     var thickness = 25 + Math.random() * 25;
+    var y1 = Math.random()*crayon.canvas.height;
+    var y1w = Math.random()*crayon.canvas.height;
+    var y2 = Math.random()*crayon.canvas.height;
+    var y2w = Math.random()*crayon.canvas.height;
+    var y11 = Math.random()*crayon.canvas.height;
+    var y11w = Math.random()*crayon.canvas.height;
+    var y12 = Math.random()*crayon.canvas.height;
+    var y12w = Math.random()*crayon.canvas.height;
     for(var i=0; i<nx; i++) {
       for(var j=0; j<ny; j++) {
         var dx = crayon.canvas.width / nx;
         var dy = crayon.canvas.height / ny;
         crayon.clipRect(dx * i, dy * j, dx, crayon.canvas.height / ny);
-        //crayon.stroke("#FF0000", thickness).fill(false).circle(crayon.canvas.width/2 + dx * i, crayon.canvas.height/2 + dy * j, crayon.canvas.width/2 - 25);
-        //crayon.stroke("#FFFFFF", thickness).fill(false).circle(crayon.canvas.width/2 + dx * i + 50, crayon.canvas.height/2 + dy * j + ddy, crayon.canvas.width/2 - 25);
-        crayon.stroke("#FF0000", thickness).fill(false).line(0, Math.random()*crayon.canvas.height, crayon.canvas.width, Math.random()*crayon.canvas.height);
-        crayon.stroke("#FFFFFF", thickness).fill(false).line(0, Math.random()*crayon.canvas.height, crayon.canvas.width, Math.random()*crayon.canvas.height);
+        var rx = Math.random() * crayon.canvas.width;
+        crayon.stroke("#FF0000", thickness).fill(false).line(0 + rx, y1, crayon.canvas.width + rx, y2);
+        crayon.stroke("#FFFFFF", thickness).fill(false).line(0 + rx, y1w, crayon.canvas.width + rx, y2w);
+        crayon.stroke("#FF0000", thickness).fill(false).line(0 + rx, y11, crayon.canvas.width + rx, y12);
+        crayon.stroke("#FFFFFF", thickness).fill(false).line(0 + rx, y11w, crayon.canvas.width + rx, y12w);
       }
     }
     //crayon.restore();
@@ -49,7 +58,7 @@ OK.Covers.push((function() {
     crayon.translate(titleX, titleY);
     crayon.style("text");
     crayon.font("Arial", titleFontSize, "bold").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(title.toUpperCase());
-    crayon.font("Arial", titleFontSize*0.85, "normal").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
+    crayon.font("Arial", titleFontSize*0.5, "normal").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
     crayon.font("Arial", authorFontSize).fill("#222222").paragraph("left", 0.25, titleWidth, true).text(author, 0, authorFontSize/2);
     var offsetY = crayon.translateY;
     crayon.restore();
@@ -60,7 +69,7 @@ OK.Covers.push((function() {
     crayon.translate(titleX, titleY);
     crayon.style("text");
     crayon.font("Arial", titleFontSize, "bold").fill("#000000").paragraph("left", 0.25, titleWidth, true).text(title.toUpperCase());
-    crayon.font("Arial", titleFontSize*0.85, "normal").fill("#999").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, titleFontSize/4);
+    crayon.font("Arial", titleFontSize*0.5, "normal").fill("#999").paragraph("left", 0.25, titleWidth, true).text(subTitle.toUpperCase(), 0, -titleFontSize*0.25);
     crayon.font("Arial", authorFontSize).fill("#222222").paragraph("left", 0.25, titleWidth, true).text(author, 0, authorFontSize/2);
     crayon.restore();
 
