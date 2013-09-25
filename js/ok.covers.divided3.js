8171a7c9000633c38a4fa9dc06dc57202ae53794
vkuchinov <me@Vladimirs-MacBook-Pro.local>
2013-09-25 20:14:45 +0100

    Typography algorithms. Should be worked out more

---
+++ js/ok.covers.divided3.js
@@ -0,0 +1,143 @@
+//LAYOUT RULES
+//
+
+
+var OK = OK || {};
+OK.Covers = OK.Covers || [];
+
+OK.Covers.push((function() {
+  var crayon;
+  var img_matrix = new Array(16);
+  var letters = new Array("", "", "", "");
+    
+ function paperAging(year) {
+    //1950 - 230/200/150
+    // 25/55/95
+
+    var currentTime = new Date();
+    var currentYear = currentTime.getFullYear();
+
+    var range = currentYear - 1950;
+    var timepos = currentYear - year;
+
+    var R_colour = 255 - Math.round(timepos / range * 25);
+    var G_colour = 255 - Math.round(timepos / range * 55);
+    var B_colour = 255 - Math.round(timepos / range * 95);
+
+    var colour_out = "rgb(" + R_colour + ", " + G_colour + ", " + B_colour + ")"
+    return colour_out;
+
+}
+    
+function renderMatrix(bm_data) {
+    for (y = 0; y < 3; y++) {
+        for (x = 0; x < 3; x++) {
+            var side_width = quarter_width;
+            //var side_width = crayon.canvas.width * 0.55;
+            crayon.context.putImageData(bm_data[Math.round(Math.random() * 15)], quarter_x + x * side_width / 2 * 1.02, quarter_y + y * side_width / 2 * 1.02);
+        }
+    }
+}
+
+function drawLetters(letters, x, y, side) {
+    
+    var draw_canvas = document.createElement("canvas");
+    draw_canvas.width = quarter_width;
+    draw_canvas.height = quarter_width;
+    var d = draw_canvas.getContext("2d");
+  
+    
+    for (var l = 0; l < 4; l++) {
+
+        d.fillStyle = "rgb(" + (200 + (25 - Math.round(Math.random() * 50))) + ", 130, 155)";
+        d.fillRect(x, y, side, side);
+
+        var fontScale = side * 1.45;
+        d.fillStyle = "#3b253c";
+        d.font = "bold " + fontScale + "px Arial";
+
+        var metrics = d.measureText(letters[l]);
+        var width = metrics.width;
+
+        d.fillText(letters[l], x + side / 2 - width / 2, y + side / 2 + fontScale / 2.85);
+
+        img_matrix[(l * 4 + 0)] = d.getImageData(x, y, side / 2, side / 2);
+        img_matrix[(l * 4 + 1)] = d.getImageData(x + side / 2, y, side / 2, side / 2);
+        img_matrix[(l * 4 + 2)] = d.getImageData(x, y + side / 2, side / 2, side / 2);
+        img_matrix[(l * 4 + 3)] = d.getImageData(x + side / 2, y + side / 2, side / 2, side / 2);
+
+    }
+    
+    //console.log("BD: " + img_matrix[0]);
+}
+    
+  function makeCover(book) {
+    if (!crayon) {
+      crayon = new Crayon( document.getElementById("cover") );
+    }
+      
+    crayon.clear();
+    crayon.style("default");
+
+    quarter_width = Math.round((crayon.canvas.width-100)/4*3)/1.5;
+    quarter_height = Math.round((crayon.canvas.height-100)/4*3)/1.5;
+    quarter_x = 100+(crayon.canvas.width-100)/4;
+    quarter_y = 50+(crayon.canvas.height-100)/4;
+
+    var author = OK.Covers.Typography.formatAuthorName(book.author);
+    var year =  book.year; 
+    
+    var paper = paperAging(year);
+    crayon.fill(paper).rect(0, 0, crayon.canvas.width, crayon.canvas.height);
+    
+    letters[0] = author[1][0].charAt(0);
+    letters[1] = author[0][0].charAt(0);
+    letters[2] = year.charAt(2);
+    letters[3] = year.charAt(3);
+      
+
+    drawLetters(letters, 0, 0, quarter_width);
+    renderMatrix(img_matrix);
+      
+    //console.log(letters);
+      
+    var authorFontSize = crayon.canvas.height * 0.03;
+    var titleFontSize = crayon.canvas.height * 0.05;
+
+    var titleX = quarter_x;
+    var titleY = quarter_y + quarter_width*1.7 + crayon.canvas.width * 0.01;
+    var titleWidth = crayon.canvas.width * 0.4;
+
+    
+    var longest = OK.Covers.Typography.longestWord(book.title);
+      
+    var titleSections = OK.Covers.Utils.breakTitle(book.title);
+    var title = titleSections[0];
+    var subTitle = titleSections[1];
+    //crayon.save();
+    crayon.context.textAlign = "left";
+    crayon.translate(titleX, titleY);
+    crayon.font("BenchNineLight", titleFontSize, "normal").fill("#000000").paragraph("left", -0.2, titleWidth, true).text(title);
+    crayon.font("BenchNineLight", titleFontSize*0.85, "normal").fill("#000000").paragraph("left", -0.2, titleWidth, true).text(subTitle, 0, titleFontSize/4);
+    //crayon.font("UbuntuCondensed", authorFontSize, author[1][1]).fill("#313131").paragraph("left", 0.25, titleWidth, false).text(author[1][0], 0, authorFontSize/2);
+    //0 + name.pixelLength of author[1][0] + " "
+    crayon.context.font=authorFontSize + "px UbuntuCondensed";
+    var tmp = crayon.context.measureText(author[0][0] + "").width;
+    var tmp1 = crayon.context.measureText(author[1][0] + "" + author[0][0]).width;
+    //crayon.restore()
+    //crayon.translate(-quarter_width*2, quarter_width/3);
+    crayon.context.textAlign = "right";
+    //crayon.context.fillText(author[1][0], crayon.canvas.width - 50 - tmp1, 54);
+    crayon.context.fillText(author[0][0], quarter_x,quarter_width/2);
+    //crayon.translate(tmp, 0);
+    //crayon.font("UbuntuCondensed", authorFontSize, author[0][1]).fill("#313131").paragraph("left", -0.25, titleWidth, true).text(author[0][0], 0, authorFontSize/2);
+
+    OK.Covers.Utils.addCover();
+  }
+
+  return {
+    enabled : false,
+    name : "Divided",
+    makeCover : makeCover
+  };
+})());
---
+++ js/ok.covers.goldentype.js
@@ -0,0 +1,196 @@
+var OK = OK || {};
+
+OK.Covers = OK.Covers || [];
+
+OK.Covers.push((function() {
+  var crayon;
+  var initials = new Array("", "");
+  var golden_width = 0;
+  var golden_height = 0;
+  var golden_x = 0;
+  var golden_y = 0;
+  var mark = 0;
+  
+
+function getPosition(imgData, width, height, mode) {
+
+    var alpha = 0;
+    var stop = false;
+
+    switch(mode)
+    {
+    case "left":
+    for (var x = 0; x < width; x += 1) {
+        for (var y = 0; y < height; y += 1) {
+            alpha = imgData.data[y * width * 4 + x * 4 + 3];
+            if (alpha < 128 && stop == false) {
+                mark[0] = y * width * 4 + x * 4;
+                mark[1] = x;
+                mark[2] = y;
+
+            } else {
+                stop = true;
+
+            }
+        }
+    }
+    break;
+    
+    case "right":
+    for (var x = 0; x < width ; x += 1) {
+        for (var y = 0; y < height-1; y += 1) {
+            alpha = imgData.data[(y + 1) * width * 4 - x*4 - 1];
+            if (alpha < 128 && stop == false) {
+                    mark[0] = y * width * 4 + x * 4;
+                    mark[1] = x;
+                    mark[2] = y;
+                    //imgData.data[(y + 1) * width * 4 - x*4 - 2] = 128;
+                    //imgData.data[(y + 1) * width * 4 - x*4 - 3] = 128; 
+
+                } else {
+                    stop = true;
+                                                  }
+        }
+    }
+    break;
+      
+    }
+    
+    return mark;
+}
+
+    
+  function goldenType (initials) 
+    {
+    
+    
+
+    //"#FF9900"
+ var tmp_canvas2 = document.createElement("canvas");
+  tmp_canvas2.setAttribute('width',golden_width);
+  tmp_canvas2.setAttribute('height',golden_height);
+  tmp_canvas2.setAttribute('id',"tmp_canvas2");
+  document.body.appendChild(tmp_canvas2);
+  var tmp2 = document.getElementById('tmp_canvas2');
+  tmpctx2 = tmp2.getContext('2d');
+  
+       
+  var tmp_canvas1 = document.createElement("canvas");
+  tmp_canvas1.setAttribute('width',golden_width);
+  tmp_canvas1.setAttribute('height',golden_height);
+  tmp_canvas1.setAttribute('id',"tmp_canvas1");
+  document.body.appendChild(tmp_canvas1);
+  var tmp1 = document.getElementById('tmp_canvas1');
+  tmpctx = tmp1.getContext('2d');
+  
+    
+    
+    tmpctx2.rect(0, 0, golden_width, golden_height);
+    tmpctx2.fillStyle = "#FFFFFF";
+    tmpctx2.fill();
+        
+    tmpctx2.font = "bold " + golden_height*1.4+ "px Arial";
+    tmpctx2.fillStyle = "#319dff";
+    tmpctx2.fillText(initials[1], 100, golden_height) 
+    var tmp2Data = tmpctx2.getImageData(0, 0, golden_width, golden_height);
+    //trim2mark = getPosition(tmp2Data, golden_width, golden_height, "left");
+    tmpctx2.rect(0, 0, golden_width, golden_height);
+    tmpctx2.fillStyle = "#FFFFFF";
+    tmpctx2.fill();
+    tmpctx2.fillStyle = "#319dff";
+    //console.log("trim2: " + trim2mark);
+    tmpctx2.fillText(initials[1], 100 , golden_height) 
+    var tmp2Data = tmpctx2.getImageData(0, 0, golden_width, golden_height);
+    //tmpctx2.fill();
+    //tmpctx2.putImageData(trim2Data, 0, 0);
+    
+        
+    tmpctx.rect(0, 0, golden_width, golden_height);
+    tmpctx.fillStyle = "rgb(242, 242, 239)";
+    tmpctx.fill();
+        
+    tmpctx.font = "bold " + golden_height*1.4+ "px Arial";
+    tmpctx.fillStyle = "#F41099";
+    tmpctx.fillText(initials[0], 0, golden_height)
+    var tmp1Data = tmpctx.getImageData(0, 0, golden_width, golden_height);
+    trim1mark = getPosition(tmp1Data, golden_width, golden_height, "left"); 
+    console.log("trim1: " + trim1mark);
+    //tmpctx.rect(0, 0, golden_width, golden_height);
+    //tmpctx.fillStyle = "#FFFFFF";
+    //tmpctx.fill();
+    //tmpctx.putImageData(trim1Data, 0, 0);
+    //var bottom_img = crayon.context.getImageData(600, 800);
+    //crayon.clear();
+    
+    
+   tmpctx.blendOnto( tmpctx2, 'multiply', {destX: 0 ,destY:0}, -200 );
+    
+   var tmpData = tmpctx2.getImageData(0,0, golden_width, golden_height);
+    document.body.removeChild(tmp_canvas2);
+    document.body.removeChild(tmp_canvas1);
+    
+    //crayon.context.fillStyle = "#0078E6";
+    //crayon.context.fillText(initials[1], golden_x + 95, crayon.canvas.height)
+    crayon.context.putImageData(tmpData, golden_x - trim1mark, golden_y);
+   
+    //crayon.clear();
+     
+    
+    
+    }
+    
+  function makeCover(book) {
+    if (!crayon) {
+      crayon = new Crayon( document.getElementById("cover") );
+    }
+
+    golden_width = Math.round(crayon.canvas.width/1.61803398875);
+    golden_height = Math.round(crayon.canvas.height/1.61803398875);
+    golden_x = crayon.canvas.width - golden_width;
+    golden_y = crayon.canvas.height - golden_height;
+      
+    //console.log("golden ratio XY: " + golden_x + ", " + golden_y);
+      
+    crayon.clear();
+    crayon.style("default");
+
+    crayon.fill("#f2f2ef").rect(0, 0, crayon.canvas.width, crayon.canvas.height);
+    //golden_box;
+    //crayon.fill("FF42E2").rect(golden_x, golden_y, golden_width, golden_height);
+
+    var author = OK.Covers.Typography.formatAuthorName(book.author);
+
+    initials[0] = author[1][0].charAt(0);
+    initials[1] = author[0][0].charAt(0);
+    
+    goldenType(initials);
+      
+    var authorFontSize = crayon.canvas.height * 0.04;
+    var titleFontSize = crayon.canvas.height * 0.03;
+    var titleFontSize = crayon.canvas.height * 0.03;
+
+    var titleX = crayon.canvas.width * 0.08;
+    var titleY = crayon.canvas.width * 0.08 + authorFontSize;
+    var titleWidth = crayon.canvas.width * 0.382;
+
+    var titleSections = OK.Covers.Utils.breakTitle(book.title);
+    var title = titleSections[0];
+    var subTitle = titleSections[1];
+
+    crayon.translate(titleX, titleY);
+    title = OK.Covers.Typography.addLigatures(title, "Arial");
+    crayon.font("Arial", titleFontSize, "bold", 0).fill("#000000").paragraph("right", 0.25, titleWidth, true).text(title);
+    crayon.font("Arial", titleFontSize*0.85, "normal", 0).fill("#000000").paragraph("left", 0.25, titleWidth, true).text(subTitle, 0, titleFontSize/4);
+    //crayon.font("Arial", authorFontSize, author[1][1], 0).fill("#FF9000").paragraph("left", 0.25, titleWidth, false).text(author[1][0], 0, authorFontSize/2);
+    //0 + name.pixelLength of author[1][0] + " "
+    //crayon.font("Arial", authorFontSize, author[0][1], 0).fill("#FF9000").paragraph("left", -0.25, titleWidth, true).text(author[0][0], 0 + crayon.measureText(author[1][0] + " ").width, authorFontSize/2);
+
+    OK.Covers.Utils.addCover();
+  }
+
+  return {
+    enabled : false,
+    name : "Golden Ratio Typography",
+    makeCover : makeCover
+  };
+})());
