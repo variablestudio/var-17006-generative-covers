57c323bad3412ce1a6ce9e600d2db595618acc71
Marcin Ignac <marcin.ignac@gmail.com>
2017-08-09 16:45:50 +0100

    Import layouts in the right order

--- index.js
+++ index.js
@@ -5,55 +5,63 @@ require('./lib/canvas.ext')
 // const patterns = require('./patterns')
 // const chroma = require('chroma-js')
 const covers = [
-  require('./lib/covers/layouts/margined.js'),
+  // require('./lib/covers/layouts/simplest.js'),
+  //
+  // require('./lib/covers/layouts/pseudo.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.hip.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.penguin.js'),
+  //
+  // require('./lib/covers/layouts/moleskine.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.header.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.footer.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.semiopaque.js'),
+  // require('./lib/covers/soso-circularchaos2.js'),
+  //
+  // require('./lib/covers/ok.covers.layout.goldenlabel.js'), //TODO: text is too big, font missing
+  //
+  // require('./lib/covers/ok.covers.layout.rightlabel.js'), //TODO: patterns missing and fonts
+  // require('./lib/covers/ok.covers.layout.hugetype.js'), //TODO: blendOnto is missing
+  //
+  // require('./lib/covers/ok.covers.layout.goldentype.js'), //TODO: blendOnto is not a function, text out
+  //
+  // require('./lib/covers/layouts/swissquad.js'),
+  // require('./lib/covers/divided3.js'), // kind of swissquad
+  //
+  // require('./lib/covers/divided.js'),
+  //
+  // require('./lib/covers/ok.covers.mosaic.js'), //TODO: font missing
+  //
+  // require('./lib/covers/layouts/margined.js'),
+  //
+  // require('./lib/covers/layouts/cornered.js'),
+  // require('./lib/covers/divided2.js'), // kind of cornered
+  // require('./lib/covers/goldencorner.js'),// kind of cornered
+  // require('./lib/covers/ok.covers.layout.hugetype2.js'),//TODO: blendOnto is missing
+  //
+  // require('./lib/covers/ok.covers.layout.goldentree.js'), //TODO: patterns missin
+  //
+  // require('./lib/covers/fourth.js'),
 
-  require('./lib/covers/layouts/swissquad.js'),
-  require('./lib/covers/divided3.js'), // kind of swissquad
+  // // Nice but not from matrix
+  // require('./lib/covers/layout.charts.js'),  //TODO: text sticks out
+  // require('./lib/covers/ok.covers.layout.stitch.js'), //TODO fonts missing, nice
+  // require('./lib/covers/layout.bonedupl.js'),  //TODO: limit available fonts
+  // require('./lib/covers/data.js'),
+  // require('./lib/covers/ok.covers.layout.mapper.js'), // cool triangles
+  // require('./lib/covers/ok.covers.layout.grids.js'), // cool, no text though?
+  // require('./lib/covers/ok.covers.layout.wavecircles.js'), //TODO: text missing, cool patterns
+  // require('./lib/covers/ok.covers.rule2.js'), // cool
 
-  require('./lib/covers/layouts/cornered.js'),
-  require('./lib/covers/goldencorner.js'),// kind of cornered
-  require('./lib/covers/divided2.js'), // kind of cornered
-
-  require('./lib/covers/fourth.js'),
-
-  require('./lib/covers/layouts/moleskine.js'),
-  require('./lib/covers/layouts/simplest.js'),
-  require('./lib/covers/layouts/pseudo.js'),
-  require('./lib/covers/ok.covers.layout.penguin.js'), //TODO: patterns missing and fonts
-  require('./lib/covers/ok.covers.layout.rightlabel.js'), //TODO: patterns missing and fonts
-  require('./lib/covers/ok.covers.layout.goldentype.js'), //TODO: blendOnto is not a function, text out
-  require('./lib/covers/divided.js'),
-  require('./lib/covers/ok.covers.layout.goldentree.js'), //TODO: patterns missin
-
-  require('./lib/covers/ok.covers.layout.footer.js'), //TODO: text is too big, font missing
-  require('./lib/covers/ok.covers.layout.header.js'), //TODO: patterns missing
-  require('./lib/covers/ok.covers.layout.goldenlabel.js'), //TODO: text is too big, font missing
-  require('./lib/covers/ok.covers.layout.hip.js'), //TODO: patterns missing, cool hex
-
-  require('./lib/covers/ok.covers.layout.semiopaque.js'), //TODO: patterns missing and fonts
-  require('./lib/covers/soso-circularchaos2.js'),
-
-  require('./lib/covers/ok.covers.mosaic.js'), //TODO: font missing
-
-  require('./lib/covers/ok.covers.layout.hugetype.js'), //TODO: blendOnto is missing
-  require('./lib/covers/ok.covers.layout.hugetype2.js'),//TODO: blendOnto is missing
-
-  // Nice but not from matrix
-  require('./lib/covers/layout.charts.js'),  //TODO: text sticks out
-  require('./lib/covers/ok.covers.layout.stitch.js'), //TODO fonts missing, nice
-  require('./lib/covers/layout.bonedupl.js'),  //TODO: limit available fonts
-  require('./lib/covers/data.js'),
-  require('./lib/covers/ok.covers.layout.mapper.js'), // cool triangles
-  require('./lib/covers/ok.covers.layout.grids.js'), // cool, no text though?
-  require('./lib/covers/ok.covers.layout.wavecircles.js'), //TODO: text missing, cool patterns
-  require('./lib/covers/ok.covers.rule2.js'), // cool
-
-  // Hmm
-  require('./lib/covers/kaleidoscope.js'),
-  require('./lib/covers/layout.layered-cube.js'), //TODO: no text?
+  // // Hmm
+  // require('./lib/covers/kaleidoscope.js'),
+  // require('./lib/covers/layout.layered-cube.js'), //TODO: no text?
 
   // Todo -------
-
 
   // require('./lib/covers/soso-blend.js'),
   // require('./lib/covers/soso-blend2.js'),
@@ -107,27 +115,31 @@ const numCovers = 6 * covers.length
 
 window.onload = function () {
   fonts.load((err, fonts) => {
-    var i = 0
-    function next() {
-      const book = books[i % books.length]
-      //let cover = random.element(covers)
-      const cover = covers[Math.floor(i / 6)]
-      if (i % 6 == 0) {
-        var header = document.createElement('h2')
-        header.innerText = cover.name
-        header.style.clear = 'both'
-        header.style.margin = '0'
-        header.style.padding = '1em 0 0 0.5em'
-        header.style.fontFamily = 'Verdana'
-        header.style.color = '#FFF'
-        document.getElementById('covers').appendChild(header)
+    try {
+      var i = 0
+      function next() {
+        const book = books[i % books.length]
+        //let cover = random.element(covers)
+        const cover = covers[Math.floor(i / 6)]
+        if (i % 6 == 0) {
+          var header = document.createElement('h2')
+          header.innerText = cover.name
+          header.style.clear = 'both'
+          header.style.margin = '0'
+          header.style.padding = '1em 0 0 0.5em'
+          header.style.fontFamily = 'Verdana'
+          header.style.color = '#FFF'
+          document.getElementById('covers').appendChild(header)
+        }
+        cover.makeCover(book)
+        i++
+        if (i < numCovers) {
+          setTimeout(next, 10)
+        }
       }
-      cover.makeCover(book)
-      i++
-      if (i < numCovers) {
-        setTimeout(next, 10)
-      }
+      next()
+    } catch (e) {
+      console.log(e)
     }
-    next()
   })
 }
