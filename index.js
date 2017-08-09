f141f8e44cd7938dec6970e15a243d393d32a7d3
Marcin Ignac <marcin.ignac@gmail.com>
2017-08-09 17:41:46 +0100

    Clean up cover naming and remove unused

--- index.js
+++ index.js
@@ -5,61 +5,62 @@ require('./lib/canvas.ext')
 // const patterns = require('./patterns')
 // const chroma = require('chroma-js')
 const covers = [
-  // require('./lib/covers/layouts/simplest.js'),
+  require('./lib/covers/layouts/simplest.js'),
   //
-  // require('./lib/covers/layouts/pseudo.js'),
+  require('./lib/covers/layouts/pseudo.js'),
   //
-  // require('./lib/covers/ok.covers.layout.hip.js'),
+  // require('./lib/covers/layout.hip.js'),
   //
-  // require('./lib/covers/ok.covers.layout.penguin.js'),
+  // require('./lib/covers/layout.penguin.js'),
   //
-  // require('./lib/covers/layouts/moleskine.js'),
+  require('./lib/covers/layouts/moleskine.js'),
   //
-  // require('./lib/covers/ok.covers.layout.header.js'),
+  // require('./lib/covers/layout.header.js'),
   //
-  // require('./lib/covers/ok.covers.layout.footer.js'),
+  // require('./lib/covers/layout.footer.js'),
   //
-  // require('./lib/covers/ok.covers.layout.semiopaque.js'),
-  // require('./lib/covers/soso-circularchaos2.js'),
+  // require('./lib/covers/layout.semiopaque.js'),
+  // require('./lib/covers/layout.semiopaque.circularchaos2.js'),
   //
-  // require('./lib/covers/ok.covers.layout.goldenlabel.js'), //TODO: text is too big, font missing
+  // require('./lib/covers/layout.goldenlabel.js'), //TODO: text is too big, font missing
   //
-  // require('./lib/covers/ok.covers.layout.rightlabel.js'), //TODO: patterns missing and fonts
-  // require('./lib/covers/ok.covers.layout.hugetype.js'), //TODO: blendOnto is missing
+  // require('./lib/covers/layout.rightlabel.js'), //TODO: patterns missing and fonts
+  // require('./lib/covers/layout.hugetype.js'), //TODO: blendOnto is missing
   //
-  // require('./lib/covers/ok.covers.layout.goldentype.js'), //TODO: blendOnto is not a function, text out
+  // require('./lib/covers/layout.goldentype.js'), //TODO: blendOnto is not a function, text out
   //
-  // require('./lib/covers/layouts/swissquad.js'),
+  require('./lib/covers/layouts/swissquad.js'),
   // require('./lib/covers/divided3.js'), // kind of swissquad
   //
   // require('./lib/covers/divided.js'),
   //
-  // require('./lib/covers/ok.covers.mosaic.js'), //TODO: font missing
+  // require('./lib/covers/mosaic.js'), //TODO: font missing
   //
-  // require('./lib/covers/layouts/margined.js'),
+  require('./lib/covers/layouts/margined.js'),
   //
-  // require('./lib/covers/layouts/cornered.js'),
+  require('./lib/covers/layouts/cornered.js'),
   // require('./lib/covers/divided2.js'), // kind of cornered
   // require('./lib/covers/goldencorner.js'),// kind of cornered
-  // require('./lib/covers/ok.covers.layout.hugetype2.js'),//TODO: blendOnto is missing
+  // require('./lib/covers/layout.hugetype2.js'),//TODO: blendOnto is missing
   //
-  // require('./lib/covers/ok.covers.layout.goldentree.js'), //TODO: patterns missin
+  // require('./lib/covers/layout.goldentree.js'), //TODO: patterns missin
   //
   // require('./lib/covers/fourth.js'),
-
+  //
   // // Nice but not from matrix
   // require('./lib/covers/layout.charts.js'),  //TODO: text sticks out
-  // require('./lib/covers/ok.covers.layout.stitch.js'), //TODO fonts missing, nice
+  // require('./lib/covers/layout.stitch.js'), //TODO fonts missing, nice
   // require('./lib/covers/layout.bonedupl.js'),  //TODO: limit available fonts
   // require('./lib/covers/data.js'),
-  // require('./lib/covers/ok.covers.layout.mapper.js'), // cool triangles
-  // require('./lib/covers/ok.covers.layout.grids.js'), // cool, no text though?
-  // require('./lib/covers/ok.covers.layout.wavecircles.js'), //TODO: text missing, cool patterns
+  // require('./lib/covers/layout.mapper.js'), // cool triangles
+  // require('./lib/covers/layout.grids.js'), // cool, no text though?
+  // require('./lib/covers/layout.wavecircles.js'), //TODO: text missing, cool patterns
   // require('./lib/covers/ok.covers.rule2.js'), // cool
 
   // // Hmm
   // require('./lib/covers/kaleidoscope.js'),
   // require('./lib/covers/layout.layered-cube.js'), //TODO: no text?
+  // require('./lib/covers/hexagons.js'), //text is out
 
   // Todo -------
 
@@ -75,7 +76,7 @@ const covers = [
   // require('./lib/covers/todo-layout.circles.js'),
 
   // require('./lib/covers/x-curves.js'),
-  // require('./lib/covers/x-hexagons.js'), //text is out
+
   // require('./lib/covers/x-hexagons.old.js'), //TODO: text outside of text bg
   // require('./lib/covers/x-layout.swissquad.js'), //TODO: no text bg
   // require('./lib/covers/x-ok.covers.layout.divided2.js'), //TODO: text sticks out
@@ -110,8 +111,9 @@ coversContainer.id = 'covers'
 document.body.appendChild(coversContainer)
 
 console.log('covers', covers.length)
-random.seed(0)
-const numCovers = 6 * covers.length
+//random.seed(0)
+const numCoversPerAlgo = 6
+const numCovers = numCoversPerAlgo * covers.length
 
 window.onload = function () {
   fonts.load((err, fonts) => {
@@ -120,8 +122,8 @@ window.onload = function () {
       function next() {
         const book = books[i % books.length]
         //let cover = random.element(covers)
-        const cover = covers[Math.floor(i / 6)]
-        if (i % 6 == 0) {
+        const cover = covers[Math.floor(i / numCoversPerAlgo)]
+        if (i % numCoversPerAlgo == 0) {
           var header = document.createElement('h2')
           header.innerText = cover.name
           header.style.clear = 'both'
@@ -131,7 +133,7 @@ window.onload = function () {
           header.style.color = '#FFF'
           document.getElementById('covers').appendChild(header)
         }
-        cover.makeCover(book)
+        cover.makeCover(book, (i % numCoversPerAlgo) % 6)
         i++
         if (i < numCovers) {
           setTimeout(next, 10)
