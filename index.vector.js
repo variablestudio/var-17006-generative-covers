d2711379f816bbdb69b1f0bc8f668954bc59e524
Marcin Ignac <marcin.ignac@gmail.com>
2017-08-13 00:41:57 +0100

    First try at porting mapper algorithm

--- index.vector.js
+++ index.vector.js
@@ -7,6 +7,7 @@ const books = require('./data/books.json')
 const rule2 = require('./lib/patterns/rule2')
 const waveCircles = require('./lib/patterns/wave-circles')
 const grids = require('./lib/patterns/grids')
+const mapper = require('./lib/patterns/mapper')
 
 random.seed(0)
 
@@ -88,79 +89,10 @@ function makeCover (book, font) {
 
   // rule2(r, W, H, book)
   // waveCircles(r, W, H, book)
-  grids(r, W, H, book)
+  // grids(r, W, H, book)
+  mapper(r, W, H, book)
 
   /*
-  var minPageCount = 10
-  var maxPageCount = 500
-
-  var borderScale = 1 + Math.random()
-  var hue = Math.random() * 360
-    // var numX = 2 + Math.floor(Math.random() * 10);
-  var numX = utils.remap(book.pageCount, minPageCount, maxPageCount, 2, 17, true)
-  // numX = 5
-  var radius = (0.2 + 0.5 * Math.random())
-  // radius = 1
-  var radius2 = (0.1 + 0.4 * Math.random())
-  // radius2 = 0.5
-  var counterLines = (Math.random() > 0.5) ? 0.1 : 0
-
-  var niceBlue = '#27D1E7'
-  var colorHSL = chroma.hex(niceBlue).hsl()
-  var color = chroma.hsl(hue, 0.7, colorHSL[2] * 1.1).hex()
-  var lightColor = chroma.hsl(hue, 0.3, colorHSL[2] * 1.8).hex()
-
-  // bg
-  r.rect(0, 0, W, H).stroke(false).fill(color)
-
-  var stepX = r.width / (numX + 1)
-  var stepY = stepX
-  var r1 = stepX * radius
-  var r2 = stepX * radius2 * 0.25
-
-  console.log(stepX, r1, r2)
-  // crayon.fill(lightColor).stroke(false)
-
-  for (var x = 0; x <= r.width + stepX / 2; x += stepX) {
-    for (var y = 0; y <= r.height + stepY / 2; y += stepY) {
-      // break
-      var leftOrRight = Math.floor(Math.random() * 2)
-      // crayon.save()
-      // crayon.translate(x, y)
-      // crayon.scale(borderScale, borderScale)
-      let g = r.group(x, y)
-      if (leftOrRight) {
-        r.rect(-r1, -r2, r1 * 2, r2 * 2)
-          .rotate(-45)
-          .stroke(false)
-          .fill(lightColor)
-          .addTo(g)
-        r.rect(-r1, -r2 * counterLines, r1 * 2, r2 * 2 * counterLines)
-          .rotate(45)
-          .stroke(false)
-          .fill(lightColor)
-          .addTo(g)
-      }
-      if (!leftOrRight) {
-        r.rect(-r1, -r2 * counterLines, r1 * 2, r2 * 2 * counterLines)
-          .rotate(-45)
-          .stroke(false)
-          .fill(lightColor)
-          .addTo(g)
-        r.rect(-r1, -r2, r1 * 2, r2 * 2)
-          .rotate(45)
-          .stroke(false)
-          .fill(lightColor)
-          .addTo(g)
-        // crayon.rect(-r, -r2 * counterLines, r * 2, r2 * 2 * counterLines)
-        // .rotate(90).rect(-r, -r2, r * 2, r2 * 2)
-      }
-      g.scale(borderScale)
-      // crayon.restore()
-    }
-  }
-  */
-
   var author = typo.formatAuthorName(book.author)
 
   var authorFontSize = r.height * 0.03
@@ -177,7 +109,6 @@ function makeCover (book, font) {
   var titleLines = breakLines(font, book.title, titleWidth, { fontSize: titleFontSize })
   var titleMeasurements = measureText(font, titleLines, { fontSize: titleFontSize })
   var titleAscent = -titleMeasurements.y
-  console.log('titleMeasurements', titleMeasurements)
 
   titleY += titleAscent
 
@@ -211,6 +142,8 @@ function makeCover (book, font) {
     .fill('#333333')
     .fontSize(authorFontSize)
     .fontFamily('PTSans')
+
+  */
   r.draw()
   return r.el
 }
